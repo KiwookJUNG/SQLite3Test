6321b3f316494ef8e624fed05c0a15c60c2adb9f
정기욱 <rldnr56@me.com>
2019-02-23 19:16:19 +0900

    1. sql 구문을 작성해준다. viewDidLoad에 작성해야 하기 때문에 IF NOT EXISTS를 붙여준다. ( 메모리에 로딩될 떄마다 반복해서 호출되면 안되므로 )
    2. slqite3_open 함수로 DB를 연결해준다. 이때 첫번째 변수는 SQLite의 파일의 경로이고 두번째 인자는 DB객체를 받아줄 변수인데 구조체로 작성되있으므로 참조 변수를 이용한다.
    
    3. 다음으로 sqlite3_prepare 메소드를 사용해 SQL 구문을 전달할 준비를 한다. 이 과정에서 SQL 구문 객체인 stmt가 생성된다
    
    4. sqlite3_step 메소드를 이용해 SQL구문 객체를 db에 전달한다.
    여기서 stmt객체 안에 컴파일된 sql뿐만 아니라 DB연결 정보까지 포함되 있으므로 sqlite3객체를 인자값으로 전달할 필요가 없다.
    
    5. finalize메소드를 이용해 stmt를 해제한다.
    
    6. sqlite3_close메소드를 이용해 db연결을 종료한다.

--- SQLite3Test/ViewController.swift
+++ SQLite3Test/ViewController.swift
@@ -22,7 +22,54 @@ class ViewController: UIViewController {
         
         let dbPath = docPathURL.appendingPathComponent("db.sqlite").path
         // URL 객체에 "db.sqlite" 파일 경로를 추가한 SQLite3 데이터 베이스 경로를 만들어낸다.
+        
+        // FileManager()를 사용하지 않고 커스텀 프로퍼티 리스트를 다룰 때 사용했던
+        // let path = NSSearchPathForDirectoriesInDomains(.documnetDirectory, .userDomainMasK, true)[0] as NSStirng
+        // let dbPath = path.strings(byAppendingPaths: ["db.sqlite"])[0]
+        // 와 동일하게 사용 할 수 있음.
+        
+        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
+        // CREATE로 시작하는 구문은 데이터베이스 테이블을 정의하는 SQL 구문으로써 DDL에 해당한다.
+        // 즉, sequence라는 이름의 테이블을 정의하라. 라는 뜻
+        // 하나의 INTEGER 타입 칼럼을 가지며 칼럼명은 num
+        // viewDidLoad는 앱이 재실행되거나, 화면이 다시 메모리에 로딩될 때 반복해서 호출되므로
+        // IF NOT EXISTS를 사용하여 테이블이 없는 경우에만 새로 테이블을 생성하도록 처리해준다.
+        
+        // sqlite3 데이터베이스 관련 함수를 순서대로 호출한다.
+        if sqlite3_open(dbPath, &db) == SQLITE_OK { // libsqlite3 라이브러리에 정의된 함수들은 정상적으로 실행되었을 때
+        // 공통적으로 SQLITE_OK 상수를 반환한다.
+        // DB를 연결, db객체 생성
+        // 첫번째 인자, SQLite 파일의 경로, 두번째 인자 생성된 DB객체를 담을 변수
+        // &db 참조 타입이 사용되는 이유는 OpaquePointer가 구조체 이기 떄문이다.
     
+            if sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK {
+            // SQL구문을 전달할 준비를 한다. 이 과정에서 컴파일된 SQL 구문 객체가 생성됨
+            // stmt는 출력값 성격을 가지는 인자값, sql구문은 데이터베이스에 전달할 수 있는 sqltie3_stmt객체로 생성됨
+        
+        
+            sqlite3_step(stmt) // 컴파일된 SQL 구문 객체를 db에 전달한다.
+            // 생성된 객체를 stmt 인자값에 담아 전달한다
+            // 생성된 stmt객체 안에는 컴파일된 SQL뿐만 아니라 DB연결 정보까지 포함되어있으므로, sqlite3 객체를 인자값으로
+            // 전달할 필요 없다.
+        
+                if sqlite3_finalize(stmt) == SQLITE_DONE {
+                   // 컴파일된 SQL 구문 객체를 삭제한다. 이 과정에서 stmt가 해제된다.
+                   // SQL 실행이 끝나면 더 이상 사용하지 않는 자원, 즉 DB연결 객체와 SQL 컴파일 객체를 해제해 주어야함.
+                   // 그래야 데이터베이스가 또 다른 연결 요청을 받을 수 있다.
+                   // 자원을 해제 할때의 순서는 self.stmt -> self.db 순서로 해제한다.
+                    print("Create Table Success!")
+                    
+                }
+            } else {
+                print("Prepare Statment Fail")
+    
+            }
+        sqltie3_close(db)
+        // DB연결을 종료한다. 이 과정에서 db객체가 해제됨.
+        } else {
+            print("Database Connect Fail")
+            return
+        }
     }
 
 
